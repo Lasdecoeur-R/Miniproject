7ae091358212b4de888ec696584c88bdf5ea8316
Rassaby Ludovic <Ludovic.rassaby@gmail.com>
2024-10-25 13:04:17 +0400

    add button popup recette

--- index.php
+++ index.php
@@ -43,6 +43,42 @@ $recettes = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Site de Recettes</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        /* Styles pour la pop-up */
+        .modal {
+            display: none; 
+            position: fixed; 
+            z-index: 1000; 
+            left: 0;
+            top: 0;
+            width: 100%; 
+            height: 100%; 
+            overflow: auto; 
+            background-color: rgba(0, 0, 0, 0.4);
+        }
+
+        .modal-content {
+            background-color: #fefefe;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+
+        .close-button {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close-button:hover,
+        .close-button:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -50,7 +86,6 @@ $recettes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         <!-- Formulaire de recherche -->
         <form class="form-inline mb-4 justify-content-center" action="index.php" method="get">
-            <!-- Zone de recherche -->
             <input class="form-control mr-sm-2" type="search" name="search" placeholder="Rechercher une recette" value="<?php echo htmlspecialchars($search); ?>">
             <button class="btn btn-outline-success" type="submit">Rechercher</button>
             
@@ -69,15 +104,14 @@ $recettes = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <a href="index.php?categorie=4&search=<?php echo htmlspecialchars($search); ?>" class="btn btn-outline-secondary <?php echo $categorie_filter == 4 ? 'active' : ''; ?>">Vietnamienne</a>
         </div>
 
-        <!-- Liste des recettes (comme avant) -->
+        <!-- Liste des recettes -->
         <div class="row">
             <?php foreach ($recettes as $recette): ?>
             <div class="col-md-4 mb-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo htmlspecialchars($recette['nom']); ?></h5>
-                        <p class="card-text"><?php echo htmlspecialchars($recette['description']); ?></p>
-                        <p class="card-text"><small class="text-muted"><?php echo htmlspecialchars($recette['categorie']); ?></small></p>
+                        <button class="btn btn-primary view-recipe" data-recipe-id="<?php echo $recette['id']; ?>">Voir la recette</button>
                     </div>
                 </div>
             </div>
@@ -85,9 +119,56 @@ $recettes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </div>
     </div>
 
+    <!-- Pop-up de recette -->
+    <div id="recipe-modal" class="modal">
+        <div class="modal-content">
+            <span class="close-button">&times;</span>
+            <h2 id="recipe-title"></h2>
+            <h4>Ingrédients :</h4>
+            <ul id="recipe-ingredients"></ul>
+            <h4>Étapes de préparation :</h4>
+            <ol id="recipe-steps"></ol>
+        </div>
+    </div>
+
     <!-- Bootstrap JS and dependencies -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        // JavaScript pour gérer la pop-up
+        $(document).ready(function() {
+            // Lorsque vous cliquez sur "Voir la recette"
+            $('.view-recipe').click(function() {
+                var recipeId = $(this).data('recipe-id');
+                
+                // Requête AJAX pour récupérer les informations de la recette
+                $.ajax({
+                    url: 'get_recipe.php', // Remplacez par le fichier PHP qui récupère les recettes
+                    type: 'GET',
+                    data: { id: recipeId },
+                    success: function(response) {
+                        var recipe = JSON.parse(response);
+                        
+                        // Remplir la pop-up avec les données
+                        $('#recipe-title').text(recipe.nom);
+                        $('#recipe-ingredients').empty().append('<li>' + recipe.ingredients.replace(/,/g, '</li><li>') + '</li>');
+                        $('#recipe-steps').empty().append('<li>' + recipe.etapes.replace(/\./g, '</li><li>') + '</li>');
+
+                        // Afficher la pop-up
+                        $('#recipe-modal').show();
+                    },
+                    error: function() {
+                        alert('Erreur lors de la récupération des données de la recette.');
+                    }
+                });
+            });
+
+            // Fermer la pop-up
+            $('.close-button').click(function() {
+                $('#recipe-modal').hide();
+            });
+        });
+    </script>
 </body>
 </html>
